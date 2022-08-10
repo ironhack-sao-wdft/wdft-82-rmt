52c4806a6560dbbab5f02e7097706b2d49fffd01
Alexandre Lima <alexandreatlima@gmail.com>
2022-08-10 10:59:58 -0300

    aula js

---
+++ md01/js-intro/loop.js
@@ -0,0 +1,16 @@
+// O Loop vai executar um pedaço de codigo enquanto uma condição for verdadeira
+
+const names = ["Alexandre", "Lucas", "Igor", "Armando", "Maxwell"];
+
+// for indexado
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i].toUpperCase());
+}
+
+if (2 === 2) {
+  const exemplo = "exemplo";
+
+  console.log(exemplo);
+}
+console.log(exemplo);
---
+++ md01/js-intro/script.js
@@ -0,0 +1,87 @@
+console.log("Oi");
+
+// Constantes
+
+const name = "Alexandre";
+
+// Let -  let it change
+
+let age = 26;
+
+let isCute = true;
+
+// Tipos de dados no JS
+
+// Strings
+
+const str = "String com aspas duplas";
+const str2 = "Aspas simples, CUIDADO";
+const str3 = `Meu nome é ${name}`;
+const str4 = "12";
+
+console.log(str3);
+
+console.log(str2.length);
+console.log(str.toUpperCase());
+
+// Numbers
+
+const num = 50;
+
+console.log(num * 2);
+
+console.log(typeof (num + str4));
+
+console.log(2 + true);
+
+// Booleans
+
+const isLovely = false;
+
+// Operadores condicionais
+
+// ==
+// ===
+// !
+
+// !=
+
+// !==
+
+// >
+// >=
+
+// <
+// <=
+
+// Operadores para mais de uma condição
+
+// condição || condição
+// condição && condição
+
+if (condição) {
+  // Se a condição for TRUE, a gente executa o que ta aqui
+}
+
+if (condição) {
+} else {
+  // Se a condição for FALSE, a gente executa o que está aqui
+}
+
+if (condição) {
+} else if (condição) {
+} else {
+}
+
+// Exemplo
+
+if (age < 18) {
+  console.log("Você é menor de idade");
+}
+
+// Obs: A gente pode guardar FUNÇÕES em variáveis
+
+// SOON
+
+// Arrays
+// Objetos
